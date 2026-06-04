f4b1d9539372e11595395fc65c3898360ede64af
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-06-04 13:11:01 -0400

    Break out test that cannot be reused in different scenario into separate file
    Also go ahead and take the opportunity to clean up and modernize the
    existing tests according to our best practices.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,7 @@
-"""Module containing the tests for the default scenario."""
+"""Module containing the generic tests for the default scenario.
+
+These tests can be reused across scenarios.
+"""
 
 # Standard Python Libraries
 import os
@@ -12,64 +15,32 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize(
-    "pkg", ["apache2", "libapache2-mod-auth-gssapi", "libapache2-mod-authnz-pam"]
-)
-def test_debian_packages(host, pkg):
+def test_packages(host):
     """Test that the appropriate packages were installed."""
-    if (
-        host.system_info.distribution == "debian"
-        or host.system_info.distribution == "ubuntu"
-        or host.system_info.distribution == "kali"
-    ):
-        assert host.package(pkg).is_installed
+    distribution = host.system_info.distribution
+    if distribution in ["debian", "kali", "ubuntu"]:
+        pkgs = ["apache2", "libapache2-mod-auth-gssapi", "libapache2-mod-authnz-pam"]
+    elif distribution in ["amzn", "fedora", "redhat"]:
+        pkgs = [
+            "httpd",
+            "mod_auth_gssapi",
+            "mod_authnz_pam",
+            "mod_proxy_html",
+            "mod_session",
+            "mod_ssl",
+        ]
+    else:
+        raise ValueError(f"Unknown distribution {distribution}")
 
-
-@pytest.mark.parametrize(
-    "pkg",
-    [
-        "httpd",
-        "mod_auth_gssapi",
-        "mod_authnz_pam",
-        "mod_proxy_html",
-        "mod_session",
-        "mod_ssl",
-    ],
-)
-def test_redhat_packages(host, pkg):
-    """Test that the appropriate packages were installed."""
-    if (
-        host.system_info.distribution == "redhat"
-        or host.system_info.distribution == "fedora"
-        or host.system_info.distribution == "amzn"
-    ):
-        assert host.package(pkg).is_installed
-
-
-def test_services(host):
-    """Test that the expected services were enabled."""
-    service = None
-    if (
-        host.system_info.distribution == "debian"
-        or host.system_info.distribution == "ubuntu"
-        or host.system_info.distribution == "kali"
-    ):
-        service = "apache2"
-    elif (
-        host.system_info.distribution == "redhat"
-        or host.system_info.distribution == "fedora"
-        or host.system_info.distribution == "amzn"
-    ):
-        service = "httpd"
-
-    assert host.service(service).is_enabled
+    for pkg in pkgs:
+        assert host.package(pkg).is_installed, f"System package {pkg} not installed"
 
 
 @pytest.mark.parametrize("f", ["/usr/local/sbin/01_setup_http_service.sh"])
 def test_files(host, f):
     """Test that the expected files were installed."""
-    assert host.file(f).exists
-    assert host.file(f).is_file
-    assert host.file(f).user == "root"
-    assert host.file(f).group == "root"
-    assert host.file(f).mode == 0o500
+    assert host.file(f).exists, f"File {f} does not exist"
+    assert host.file(f).is_file, f"{f} is not a file"
+    assert host.file(f).user == "root", f"File {f} does not have user root"
+    assert host.file(f).group == "root", f"File {f} does not have group root"
+    assert host.file(f).mode == 0o500, f"File {f} does not have mode 0o500"
